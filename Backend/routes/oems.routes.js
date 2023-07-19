65e4212fcfaacfba2914108525007c7403a6e475
wave2211 <sagarikanikhade073@gmail.com>
2023-07-19 15:08:02 +0530

    complete backend

--- Backend/routes/oems.routes.js
+++ Backend/routes/oems.routes.js
@@ -6,7 +6,16 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 oemsRouter.get("/", async(res,req)=>{
-
+   try {
+      const oems = await OemsModel.findOne({ name_of_the_model, year_of_the_model});
+      if (!oems) {
+        res.send({ msg: 'Not Found!' });
+      } else {
+        res.send({ data: oems });
+      }
+    } catch (error) {
+      res.send({"msg":err.message});
+    }
 })
 
 oemsRouter.post("/add",async(req,res)=>{
